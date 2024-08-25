32ba00b7fb08cf29ea7472e374460ffc809f54fb
mandshurica831 <mandshurica831@gmail.com>
2024-08-25 21:52:33 +0900

    404はindex.htmlにする

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import Icons from 'unplugin-icons/vite'
 import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 
+import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'portfolio',
@@ -23,5 +25,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      },
+    },
+  },
 })
