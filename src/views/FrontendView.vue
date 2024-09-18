2a433a76259efd597a962907e4b5afa163880b07
mandshurica831 <mandshurica831@gmail.com>
2024-09-18 10:11:54 +0900

    フロントエンドページ調整

--- src/views/FrontendView.vue
+++ src/views/FrontendView.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
     <h1>Frontend</h1>
-
-    <div class="bg-gray">調整中</div>
-
-    <p>
-      当サイト(Vue.jsで制作)と<a
-        href="https://drive.google.com/drive/folders/1BHQdPYnT4qvHH-F5n2Kqkv4-fmY3a8zw?usp=drive_link"
-        >Google Driveのファイル</a
-      >をご覧下さい。
-    </p>
+    <ul class="bg-gray text">
+      <li><a target="_blank" href="https://github.com/mandshurica831/portfolio">当サイトリポジトリ(Vue.js)</a></li>
+      <li><a target="_blank" 
+        href="https://drive.google.com/file/d/1OSTnlM1pTPEzpf0l36fyhcPcb40EPRGV/view?usp=drive_link"
+        >色押しゲームモック (HTML/CSS/JS,jquery)</a
+      ></li>
+      <li><a target="_blank" 
+        href="https://drive.google.com/file/d/1VyPkdhK66nxjO3a0r8HmsPXYeNECLKA0/view?usp=drive_link"
+        >5秒当てゲーム (HTML/CSS/JS,jquery)</a
+      ></li>
+      <li><a target="_blank" 
+        href="https://drive.google.com/file/d/196ED8OIBxDo7QwoPIWE56YXFg6vYnOfi/view?usp=drive_link"
+        >あみだくじ (HTML/CSS/JS)</a
+      ></li>
+      <li><a target="_blank" 
+        href="https://drive.google.com/file/d/1e2VYDg95hf4k4BQcGfE3HACnTHWeiVGN/view?usp=drive_link"
+        >メモ帳 (HTML/CSS/JS)</a
+      ></li>
+      <li><a target="_blank" 
+        href="https://drive.google.com/drive/folders/1RX2UOUG9vW-ZUnRdyxf3PnBTCVfJrwcn?usp=drive_link"
+        >ログインボーナス演出 (Adobe Animate)</a
+      ></li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
-div {
-  height: 10em;
-  font-size: larger;
-  word-break: break-word;
-}
-p {
-  margin: 3em;
-  font-size: smaller;
-}
 </style>
